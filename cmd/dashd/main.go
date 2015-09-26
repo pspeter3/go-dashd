e30852f34e70772df9b19526fc516f3acdeda754
Phips Peter <pspeter333@gmail.com>
2015-09-26 08:42:25 -0700

    Add better cli
    This now includes the full set of future CLI options.

--- cmd/dashd/main.go
+++ cmd/dashd/main.go
@@ -8,20 +8,47 @@ import (
 	"log"
 )
 
-func main() {
-	var device string
-	flag.StringVar(&device, "device", "", "Device to listen on")
+type flags struct {
+	key     string
+	device  string
+	snaplen int
+	promisc bool
+	filter  string
+	verbose bool
+}
+
+func parse() flags {
+	var args flags
+	flag.StringVar(&args.key, "key", "", "IFTTT Maker Channel key")
+	flag.StringVar(&args.device, "device", "en0", "Device to listen on")
+	flag.IntVar(&args.snaplen, "snaplen", 1600, "Snaplen for pcap")
+	flag.BoolVar(&args.promisc, "promisc", true, "Use promiscuous mode")
+	flag.StringVar(&args.filter, "filter", "arp", "Pcap filter")
+	flag.BoolVar(&args.verbose, "verbose", false, "Verbose output")
 	flag.Parse()
-	handle, err := pcap.OpenLive(device, 1660, true, pcap.BlockForever)
+	return args
+}
+
+func packetSource(args flags) (chan gopacket.Packet, error) {
+	handle, err := pcap.OpenLive(args.device, int32(args.snaplen), args.promisc, pcap.BlockForever)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	err = handle.SetBPFFilter("arp")
+	err = handle.SetBPFFilter(args.filter)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	source := gopacket.NewPacketSource(handle, handle.LinkType())
-	for packet := range source.Packets() {
+	return source.Packets(), nil
+}
+
+func main() {
+	args := parse()
+	packets, err := packetSource(args)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for packet := range packets {
 		ethernet, ok := packet.LinkLayer().(*layers.Ethernet)
 		if ok {
 			log.Println(ethernet.SrcMAC)
